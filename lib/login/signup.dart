1c4fd73e4b88b11cb111df6c3efd01c6d5ba67ac
Fahad Khan Yousafzai <pak.fahadullah@gmail.com>
2023-06-04 16:47:59 +0500

    age must be 18

--- lib/login/signup.dart
+++ lib/login/signup.dart
@@ -39,27 +39,28 @@ class _SignUpState extends State<SignUp> {
   int bDMonth = DateTime.now().month;
   int bDDay = DateTime.now().day;
 
-  String yearsDropdownValue = '2023';
+  String yearsDropdownValue = '2005';
 
+  // changes
   var yearsItems = [
-    '2023',
-    '2022',
-    '2021',
-    '2020',
-    '2019',
-    '2018',
-    '2017',
-    '2016',
-    '2015',
-    '2014',
-    '2013',
-    '2012',
-    '2011',
-    '2010',
-    '2009',
-    '2008',
-    '2007',
-    '2006',
+    // '2023',
+    // '2022',
+    // '2021',
+    // '2020',
+    // '2019',
+    // '2018',
+    // '2017',
+    // '2016',
+    // '2015',
+    // '2014',
+    // '2013',
+    // '2012',
+    // '2011',
+    // '2010',
+    // '2009',
+    // '2008',
+    // '2007',
+    // '2006',
     '2005',
     '2004',
     '2003',
@@ -355,17 +356,12 @@ class _SignUpState extends State<SignUp> {
               SizedBox(
                 height: height / 100,
               ),
-              Padding(
+              Container(
                 padding: EdgeInsets.symmetric(horizontal: width / 18),
-                child: Customtextfild2.textField(
-                  _emailController,
-                  CustomStrings.emails,
-                  notifire.getdarkscolor,
-                  Padding(
-                    padding: EdgeInsets.all(height / 400),
-                    child: Image.asset("image/email.png"),
-                  ),
-                  notifire.getlightingcolor,
+                color: Colors.transparent,
+                // height: height / 16,
+                width: width,
+                child: TextFormField(
                   validator: (String? txt) {
                     bool emailValid = RegExp(
                             r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$')
@@ -378,8 +374,56 @@ class _SignUpState extends State<SignUp> {
                     }
                     return "Your Email is Wrong";
                   },
+                  controller: _emailController,
+                  style: TextStyle(color: notifire.getdarkscolor),
+                  decoration: InputDecoration(
+                    filled: true,
+                    contentPadding: EdgeInsets.zero,
+                    fillColor: notifire.getlightingcolor,
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    hintText: CustomStrings.emails,
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: height / 50,
+                    ),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.all(height / 50),
+                      child: Image.asset(
+                        "image/email.png",
+                        height: height / 46,
+                      ),
+                    ),
+                  ),
                 ),
               ),
+              // Padding(
+              //   padding: EdgeInsets.symmetric(horizontal: width / 18),
+              //   child: Customtextfild2.textField(
+              //     _emailController,
+              //     CustomStrings.emails,
+              //     notifire.getdarkscolor,
+              //     Padding(
+              //       padding: EdgeInsets.all(height / 400),
+              //       child: Image.asset("image/email.png"),
+              //     ),
+              //     notifire.getlightingcolor,
+              //     validator: (String? txt) {
+              //       bool emailValid = RegExp(
+              //               r'^.+@[a-zA-Z]+\.{1}[a-zA-Z]+(\.{0,1}[a-zA-Z]+)$')
+              //           .hasMatch(txt!);
+              //       if (txt == null || txt.isEmpty) {
+              //         return "Please provide your Email";
+              //       }
+              //       if (emailValid) {
+              //         return null;
+              //       }
+              //       return "Your Email is Wrong";
+              //     },
+              //   ),
+              // ),
               SizedBox(
                 height: height / 40,
               ),
